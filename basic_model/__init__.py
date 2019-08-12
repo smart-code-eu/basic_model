b2614f2d5f1c1ddb59e7c9be7da8595b3459294e
darianjug-sc <47183505+darianjug-sc@users.noreply.github.com>
2019-08-12 16:56:27 +0200

    Copyright update

--- basic_model/__init__.py
+++ basic_model/__init__.py
@@ -1,3 +1,22 @@
+#
+# Modifications Copyright (C) 2019 SMART CODE, razvoj aplikacij, d.o.o.
+#
+# Copyright 2016 SmartBear Software
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at [apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Original code:
+# https://github.com/swagger-api/swagger-codegen/blob/master/modules/swagger-codegen/src/main/resources/flaskConnexion/model.mustache
+#
+
 import datetime
 import pprint
 import typing
