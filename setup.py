7aed86a79c4c4313f37bc4aac99f2cd7861e7aed
Darian <darian.jug@smartcode.eu>
2019-08-13 14:23:46 +0200

    Included requirements in setup.py, updated gitignore

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="basic_model",
     version="0.0.1",
+    install_requires=requirements,
     author="(C) 2019 SMART CODE, razvoj aplikacij, d.o.o.",
     author_email="info@smartcode.eu",
     description="Serialization and deserialization made simple",
